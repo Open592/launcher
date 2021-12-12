8c46cfc07558dd0c9c83a37c055af48afb444f7b
Open592 Developer <uss@open592.com>
2021-12-12 01:28:54 -0800

    Tests: Add Launcher functional test
    This tests validates the core functionality of the launcher
    
    Signed-off-by: Open592 Developer <uss@open592.com>

--- src/tests/launcher_test.cpp
+++ src/tests/launcher_test.cpp
@@ -1,7 +1,73 @@
+#include <filesystem>
+#include <fstream>
 #include <gtest/gtest.h>
+#include <string>
 
-TEST(LauncherTest, TestAssertions)
+#include <core/Launcher.hpp>
+
+namespace fs = std::filesystem;
+
+/**
+ * The goal of this test is verify the successful execution of every part of
+ * the launcher:
+ *
+ * - Successfully able to load a parameter file when given a direct path
+ * - Successfully able to parse JVM parameters
+ * - Successfully able to initialize the JVM with the provided parameters
+ * - Successfully able to call the main method of the provided class
+ *
+ * The test is run as follows:
+ *
+ * - We verify that the parameter file exists within the `TEST_CONFIG_DIR`
+ * - Given the `TEST_CONIG_DIR` we create an exchange file which includes a
+ * known text.
+ * - We initialize the launcher with the path to the above parameter file
+ * - The mock appletviewer jar will read the exchange file and verify that
+ * it contains the known text.
+ * - The mock appletviewer jar will then replace the known text with it's own
+ * known text.
+ * - Once the launcher successfully executes we will read the exchange file and
+ * verify that the known text has been changed and is correct.
+ * - We will delete the exchange file.
+ */
+const std::string PARAMETER_FILE_NAME = "parameters.prm";
+const std::string EXCHANGE_FILE_NAME = "exchange.txt";
+const std::string SENDER_KNOWN_TEXT = "hello-from-cpp";
+const std::string EXPECTED_RECEIVER_KNOWN_TEXT = "hello-from-java";
+
+TEST(LauncherTest, VerifyJarSuccessfullyLoaded)
 {
-    EXPECT_STRNE("hello", "world");
-    EXPECT_EQ(7 * 6, 42);
+    fs::path testDataDirectory = fs::path(TEST_DATA_DIR);
+
+    EXPECT_TRUE(fs::exists(testDataDirectory));
+
+    std::ofstream exchangeFile(testDataDirectory / EXCHANGE_FILE_NAME);
+
+    EXPECT_TRUE(exchangeFile.is_open());
+
+    exchangeFile << SENDER_KNOWN_TEXT << std::endl;
+
+    exchangeFile.close();
+
+    fs::path parameterFilePath = testDataDirectory / PARAMETER_FILE_NAME;
+
+    EXPECT_TRUE(fs::exists(parameterFilePath));
+
+    Core::Launcher launcher(parameterFilePath);
+
+    EXPECT_FALSE(launcher.getClassName().empty());
+    EXPECT_FALSE(launcher.getParameters().empty());
+
+    EXPECT_NO_THROW(launcher.loadAppletViewer());
+
+    std::ifstream updatedExchangeFile(testDataDirectory / EXCHANGE_FILE_NAME);
+
+    std::string receiverKnownText;
+    std::getline(updatedExchangeFile, receiverKnownText);
+
+    EXPECT_EQ(receiverKnownText, EXPECTED_RECEIVER_KNOWN_TEXT);
+
+    updatedExchangeFile.close();
+
+    EXPECT_NO_THROW(fs::remove(testDataDirectory / EXCHANGE_FILE_NAME));
 }
--- src/tests/mocks/MockOpen592AppletViewer.java
+++ src/tests/mocks/MockOpen592AppletViewer.java
@@ -1,14 +1,68 @@
 // SPDX-License-Identifier: BSD-2-Clause
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.InvalidParameterException;
+import java.util.Scanner;
+
 public class MockOpen592AppletViewer {
-    public static String result;
-    public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            throw new Exception("Invalid argument count");
+    // The filename for the exchange file created in the cpp test
+    private static String EXCHANGE_FILE_NAME = "exchange.txt";
+
+    // The known text set by the cpp test
+    private static String EXPECTED_SENDER_KNOWN_TEXT = "hello-from-cpp";
+
+    // The known text we will set and the cpp test will subsequently read
+    private static String RECEIVER_KNOWN_TEXT = "hello-from-java";
+
+    public static void validateExchangeFile(File exchangeFile) throws IOException {
+        Scanner scanner = new Scanner(exchangeFile);
+        String senderKnownText = scanner.nextLine().strip();
+
+        scanner.close();
+
+        if (!senderKnownText.equals(EXPECTED_SENDER_KNOWN_TEXT)) {
+            String error = String.format(
+                "Expected sender known text to be: \"%s\" but received: \"%s\"",
+                senderKnownText,
+                EXPECTED_SENDER_KNOWN_TEXT
+            );
+
+            throw new IOException(error);
+        }
+    }
+
+    public static void updateExchangeFile(File exchangeFile) throws IOException {
+        FileWriter writer = new FileWriter(exchangeFile, false);
+
+        writer.write(RECEIVER_KNOWN_TEXT);
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException, IllegalArgumentException {
+        if (args.length != 0) {
+            throw new IllegalArgumentException("Expected to receive no arguments but received: " + args.length);
         }
 
-        MockOpen592AppletViewer.result = args[0];
+        // This will be the directory where the exchange file will be present
+        String testDataDir = System.getProperty("open592.test.data.dir");
 
-        System.out.println("Successfully ran MockOpen592AppletViewer with argument: " + MockOpen592AppletViewer.result);
+        if (testDataDir.isEmpty()) {
+            throw new IOException("Could not retrieve test data directory");
+        }
+
+        String exchangeFilePath = testDataDir + File.separator + MockOpen592AppletViewer.EXCHANGE_FILE_NAME;
+        File exchangeFile = new File(exchangeFilePath);
+
+        if (!exchangeFile.canRead()) {
+            throw new IOException("Cannot open the exchange file for reading");
+        }
+
+        // Make sure we received the correct known text
+        MockOpen592AppletViewer.validateExchangeFile(exchangeFile);
+
+        // Overwrite the exchange file
+        MockOpen592AppletViewer.updateExchangeFile(exchangeFile);
     }
 }
